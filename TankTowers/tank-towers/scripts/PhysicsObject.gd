c7ae5cb2d64b72d86466783d6d58eb4d467d1aa6
bugloves2code <afd2445@rit.edu>
2025-02-04 22:24:31 -0500

    added physics object class
    i think what ill do is have marine life extend physics object. physics object extends characterbody2d which is what the fish will be apart of.

---
+++ TankTowers/tank-towers/scripts/PhysicsObject.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+# last edited : Ayden Dueker 2/4
+# this class contains methods that will allow anything that extends it to calculate their movement
+# specifically right now, a Seek, or Wander behavior which can later be explanded to flocking or fleeing
+
+# right now I am determining if we should add more abstraction behavior by having the fish for example directly
+# inherit from this. I also don't want to directly put these methods in the fish class but at the same time, Marine Life
+# would have to extend this but plants dont move LOL. But its probably fine
+
+@export var maxSpeed : int
+
+var wanderAngle
+var perlinOffset
+
+var totalForce
+
+var noise: FastNoiseLite
+
+var rng = RandomNumberGenerator.new()
+
+# this method will be overrided
+func _CalcSteeringForces() -> void:
+	pass
+
+func ApplyForce(force: Vector2) -> void:
+	pass
+
+func CalcFuturePosition(time: float) -> Vector2:
+	return global_position + (velocity * time)
+
+func Seek(targetPos: Vector2) -> Vector2:
+	var desiredVel = targetPos - global_position
+	desiredVel = desiredVel.normalized() * maxSpeed
+	return desiredVel - velocity;
+
+func Wander(time: float, radius: float) -> Vector2:
+	var futurePos = CalcFuturePosition(time)
+	 wanderAngle += (0.5 - noise.get_noise_2d(global_position.x * 0.1 + perlin_offset.x, 
+											  global_position.y * 0.1 + perlin_offset.y)) * PI * delta
+	return 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	perlinOffset = rng.randf_range(0, 1000)
+	wanderAngle = rng.randf_range(0, PI * 2)
+	noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	noise.frequency = 0.1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	totalForce = Vector2.ZERO;
+	_CalcSteeringForces()
+	ApplyForce(totalForce)
