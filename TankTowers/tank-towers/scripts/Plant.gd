f81a66188d1d4c59e4e338a66201c5bd0abbc2f1
bugloves2code <afd2445@rit.edu>
2025-02-07 01:53:08 -0500

    plant

--- TankTowers/tank-towers/scripts/Plant.gd
+++ TankTowers/tank-towers/scripts/Plant.gd
@@ -5,7 +5,8 @@
 ##   and the parent node for specific plant nodes.
 
 extends MarineLife
-
+var counter
+var timePassed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super._process(delta)
+	timePassed += delta
+	if timePassed >= 1.0:
+		counter += 10
+		timePassed = 0
+		print(counter)
