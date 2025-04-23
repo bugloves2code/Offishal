82c0996e3f782b982593cbf07a46ad14ea561ee9
bugloves2code <afd2445@rit.edu>
2025-04-23 18:14:49 -0400

    title scene

--- TankTowers/tank-towers/scripts/Main.gd
+++ TankTowers/tank-towers/scripts/Main.gd
@@ -1,12 +1,26 @@
 extends Node
 @export var tutorial_manager: NodePath
+var connect: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	MusicPlayer.playTitleMusic()
+	pass
+	##get_node("/root/Main/Title").connect("gameStart", Callable(self, "gameStart"))
+
+func gameStart():
+	connect = false
 	var tutorial = get_node(tutorial_manager)
 	tutorial.start_tutorial()
-
+	MusicPlayer.playGameMusic()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if connect:
+		if has_node("/root/Main/Title"):
+			if (!get_node("/root/Main/Title").is_connected("gameStart", Callable(self, "gameStart"))):
+				get_node("/root/Main/Title").connect("gameStart", Callable(self, "gameStart"))
+			else:
+				pass
+		else:
+			pass
